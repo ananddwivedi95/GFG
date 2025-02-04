ae96d82984e694f197ea87f6b8bfbe52823e20a4
Anand Dwivedi <133527439+ananddwivedi95@users.noreply.github.com>
2025-02-05 00:24:32 +0530

    Add files via upload

---
+++ OverlappingIntervals.java
@@ -0,0 +1,39 @@
+package in.dsadec.com.GFG_DSAQuestions;
+
+import java.util.*;
+
+public class OverlappingIntervals {
+    public static void main(String[] args) {
+        int arr[][] = {{1, 3}, {2, 4}, {6, 8}, {9, 10}};
+        List<int[]> list = mergeOverlap(arr);
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(Arrays.toString(list.get(i)) + " ");
+        }
+    }
+
+    static List<int[]> mergeOverlap(int[][] arr) {
+        Arrays.sort(arr, Comparator.comparingInt(a -> a[0])); // Sort based on start time
+
+        List<int[]> ll = new ArrayList<>();
+        ll.add(arr[0]); // Add the first interval
+
+        for (int j = 1; j < arr.length; j++) {
+            int[] curr_Intervals = ll.get(ll.size() - 1); // Last interval in ll
+            int[] next_intervals = arr[j];
+
+            int cs = curr_Intervals[0];
+            int ce = curr_Intervals[1];
+
+            int ns = next_intervals[0];
+            int ne = next_intervals[1];
+
+            if (ce >= ns) { // Overlapping case
+                ll.remove(ll.size()-1);
+                ll.add(new int[]{cs,Math.max(ce,ne)}) ;// Merge in place (no remove needed)
+            } else {
+                ll.add(next_intervals); // Add non-overlapping interval
+            }
+        }
+        return ll;
+    }
+}
