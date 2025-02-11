8e1aef0e58b794205f819bbce3768cdfaa75108a
Anand Dwivedi <133527439+ananddwivedi95@users.noreply.github.com>
2025-02-12 02:42:23 +0530

    Add files via upload

--- OverlappingIntervals.java
+++ OverlappingIntervals.java
@@ -36,4 +36,45 @@ public class OverlappingIntervals {
         }
         return ll;
     }
+
+    public static interface MinCharstoAddforPalindrome {
+        public static void main(String[] args)
+        {
+            System.out.println(minChar("aacecaaaa"));
+        }
+        public static int minChar(String s) {
+            // Write your code here
+
+            int n=s.length();
+           String rev= new StringBuilder(s).reverse().toString();
+           s=s+"$"+rev;
+           int [] lps= new int[s.length()];
+           return n-lps(lps,s);
+        }
+        static int lps(int [] lps,String string)
+        {
+            int p=0;
+            int s=1;
+            while(s<string.length())
+            {
+                if(string.charAt(s)==string.charAt(p))
+                {
+                    lps[s]=p+1;
+                    s++;
+                    p++;
+                }
+                else {
+                    if(p==0)
+                    {
+                        lps[s]=0;
+                        s++;
+                    }
+                    else {
+                        p=lps[p-1];
+                    }
+                }
+            }
+            return lps[lps.length-1];
+        }
+    }
 }
