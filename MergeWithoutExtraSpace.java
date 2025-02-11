7ba73509eab03c5904b905690bb3f6cf35092605
Anand Dwivedi <133527439+ananddwivedi95@users.noreply.github.com>
2025-02-12 02:41:14 +0530

    Add files via upload

--- MergeWithoutExtraSpace.java
+++ MergeWithoutExtraSpace.java
@@ -43,4 +43,114 @@ public class MergeWithoutExtraSpace {
         a[aLength]=b[j];
         b[j]=temp;
     }
+
+    public static class StringsRotationsofEachOther {
+        public static void main(String[] args)
+        {
+            System.out.println(areRotations("aab","aba"));
+        }
+        /*static boolean areRotations(String s1, String s2) {
+            // Your code here
+            StringBuilder sb= new StringBuilder(s2);
+
+
+            //check right rotation
+            int i=0;
+            while(i<sb.length())
+            {
+                char ch=sb.charAt(0);
+                sb.deleteCharAt(0);
+                sb.append(ch);
+                System.out.println(sb);
+                if(s1.equals(sb.toString()))
+                {
+                    System.out.println("right");
+                    return true;
+                }
+                i++;
+            }
+            //check left rotation
+            int j=sb.length()-1;
+            while(j>=0)
+            {
+                char ch=sb.charAt(sb.length()-1);
+                sb.deleteCharAt(sb.length());
+                sb.insert(0,ch);
+                if(s1.equals(sb.toString()))
+                {
+                    System.out.println("left");
+                    return true;
+                }
+                j--;
+            }
+            return false;
+        }*/
+
+        public static boolean areRotations(String s1, String s2) {
+            //make this question using of kmp algorithm
+            int lps[]= new int[s2.length()]; //lpd array
+            findLps(lps,s2); //compute lps
+            return areRotationUsingKmp(s1+s1,s2,lps); //using kmp algorithm
+
+        }
+        static boolean areRotationUsingKmp(String text,String pat,int[] lps)
+        {
+            int n=text.length(); //length of text
+            int m=pat.length(); //length of pattern
+            int i=0; //track text character
+            int j=0; //track pattern character
+            while(i<text.length())
+            {
+                if(text.charAt(i)==pat.charAt(j))
+                {
+                    i++;
+                    j++;
+                    if(j==m)
+                    {
+                        return true;
+                    }
+                }
+                else
+                {
+                    if(j==0)
+                    {
+                        i++;
+                    }
+                    else
+                    {
+                        j=lps[j-1];
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        static void findLps(int []lps,String s2)
+        {
+            int p=0; //prefix
+            int s=1; //suffix
+            while(s<s2.length())
+            {
+                if(s2.charAt(p)==s2.charAt(s)) //character match
+                {
+                    lps[s]=p+1;
+                    s++;
+                    p++;
+                }
+                else //character not match
+                {
+                    if(p==0) // if p==0
+                    {
+                        lps[s]=0;
+                        s++;
+                    }
+                    else
+                    {
+                        p=lps[p-1]; //if p!=0 then p=lps[p-1]
+                    }
+                }
+            }
+        }
+    }
 }
