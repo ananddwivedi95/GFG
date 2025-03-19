18c05fe990329daf5f70cc4b1afdee99724e5655
Anand Dwivedi <133527439+ananddwivedi95@users.noreply.github.com>
2025-03-20 00:29:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sum Pair closest to target/sum-pair-closest-to-target.java
@@ -0,0 +1,82 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            ArrayList<Integer> array = new ArrayList<>();
+
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+
+            int target = Integer.parseInt(br.readLine());
+
+            Solution ob = new Solution();
+            List<Integer> res = ob.sumClosest(arr, target);
+            if (res.isEmpty()) {
+                System.out.print("[]");
+            } else {
+                for (Integer num : res) {
+                    System.out.print(num + " ");
+                }
+            }
+            System.out.println();
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public List<Integer> sumClosest(int[] arr, int target) {
+        // code here
+        int n = arr.length;
+        Arrays.sort(arr);
+        List<Integer> res = new ArrayList<>();
+        int minDiff = Integer.MAX_VALUE;
+
+        int left = 0, right = n - 1;
+
+        while (left < right) {
+            int currSum = arr[left] + arr[right];
+
+            // Check if this pair is closer than the closest
+            // pair so far
+            if (Math.abs(target - currSum) < minDiff) {
+                minDiff = Math.abs(target - currSum);
+                res = Arrays.asList(arr[left], arr[right]);
+            }
+
+            // If this pair has less sum, move to greater values
+            if (currSum < target)
+                left++;
+
+            // If this pair has more sum, move to smaller values
+            else if (currSum > target)
+                right--;
+
+            // If this pair has sum = target, return it
+            else
+                return res;
+        }
+
+        return res;
+    }
+}
