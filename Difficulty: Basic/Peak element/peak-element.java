2b3aa55e84d7020cb363bc2146f54973bf8e55d2
Anand Dwivedi <133527439+ananddwivedi95@users.noreply.github.com>
2025-04-19 22:20:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Peak element/peak-element.java
+++ Difficulty: Basic/Peak element/peak-element.java
@@ -52,21 +52,20 @@ class Solution {
 
     public int peakElement(int[] arr) {
         // code here
-        
-       int left=0;
-       int right=arr.length-1;
-       while(left<right)
-       {
-           int mid=left+(right-left)/2;
-           if(arr[mid]>=arr[mid+1])
-           {
-               right=mid;
-           }
-           else
-           {
-               left=mid+1;
-           }
-       }
-       return right;
+         int left=0;
+        int right=arr.length-1;
+        while(left<right)
+        {
+            int mid=left+(right-left)/2;
+            if(arr[mid]>=arr[mid+1])
+            {
+                right=mid;
+            }
+            else
+            {
+                left=mid+1;
+            }
+        }
+        return right;
     }
 }
